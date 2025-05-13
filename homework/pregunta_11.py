ef2ca051d070c5f2a05d733da415a5a63a240657
Christopher Obando <cobandor@unal.edu.co>
2025-05-13 16:54:39 -0500

    Solución 11

--- homework/pregunta_11.py
+++ homework/pregunta_11.py
@@ -22,3 +22,10 @@ def pregunta_11():
     38   38      d,e
     39   39    a,d,f
     """
+    import pandas as pd
+
+    df = pd.read_csv('files/input/tbl1.tsv', sep='\t')
+    df = df.sort_values(['c0', 'c4'])
+    tabla = df.groupby('c0')['c4'].apply(lambda x: ','.join(x))
+    tabla = tabla.reset_index()
+    return tabla
