4a2a8a2dead834ba0e8ce7354a9171c1c7eca1f3
Christopher Obando <cobandor@unal.edu.co>
2025-05-13 16:31:25 -0500

    Solución 2

--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -14,3 +14,8 @@ def pregunta_02():
     4
 
     """
+    import pandas as pd
+
+    df = pd.read_csv('files/input/tbl0.tsv', sep='\t')
+    numero_columnas = df.shape[1]
+    return numero_columnas
