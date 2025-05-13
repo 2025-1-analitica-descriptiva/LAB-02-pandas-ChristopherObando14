ffcd45aadca2953c1073d3f2e46f32c1a41557e1
Christopher Obando <cobandor@unal.edu.co>
2025-05-13 16:54:43 -0500

    Solución 12

--- homework/pregunta_12.py
+++ homework/pregunta_12.py
@@ -4,12 +4,12 @@ datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y
 `tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
 librerias de pandas para resolver las preguntas.
 """
-
+ 
 
 def pregunta_12():
     """
     Construya una tabla que contenga `c0` y una lista separada por ','
-    de los valores de la columna `c5a`  y `c5b` (unidos por ':') de la
+    de los valores de la columna `c5a` y `c5b` (unidos por ':') de la
     tabla `tbl2.tsv`.
 
     Rta/
@@ -22,3 +22,15 @@ def pregunta_12():
     38   38                    eee:0,fff:9,iii:2
     39   39                    ggg:3,hhh:8,jjj:5
     """
+    import pandas as pd
+
+    df = pd.read_csv('files/input/tbl2.tsv', sep='\t')
+    df = df.sort_values(['c0', 'c5a'])
+
+    df['combinado'] = df['c5a'] + ':' + df['c5b'].astype(str)
+
+    tabla = df.groupby('c0')['combinado'].apply(lambda x: ','.join(x)).reset_index()
+
+    tabla.columns = ['c0', 'c5']
+    return tabla
+
